6b7d5f9eff6dbb27b39f51082c53d57c2e68f7eb
Eugene Khumalo <eugene@lepsta.com>
2018-10-10 14:35:27 +0200

    Added Process blocks function to process data in memory

--- patch.go
+++ patch.go
@@ -5,9 +5,11 @@
 package xferspdy
 
 import (
-	"github.com/golang/glog"
+	"bytes"
 	"io"
-	"os"
+	"io/ioutil"
+
+	"github.com/golang/glog"
 )
 
 //Patch is a wrapper on PatchFile (current version only supports patching of local files)
@@ -19,16 +21,26 @@ func Patch(delta []Block, sign Fingerprint, t io.Writer) {
 // The source file would normally be the base version of the file  and
 // the Diff is the delta computed by using the Fingerprint generated for the base file and the new version of the file
 func PatchFile(delta []Block, source string, t io.Writer) error {
-	s, e := os.Open(source)
-	defer s.Close()
+	s, e := ioutil.ReadFile(source)
+	if e != nil {
+		return e
+	}
+	buf := bytes.NewReader(s)
+
+	return ProcessBlocks(delta, buf, t)
+}
+
+// ProcessBlocks takes a byte reader and applies the blocks to that data
+// This is usefull when you want to process blocks in memory as apposed to an actual file
+func ProcessBlocks(delta []Block, s *bytes.Reader, t io.Writer) error {
 	wptr := int64(0)
 	for _, block := range delta {
 		if block.HasData {
 			glog.V(3).Infof("Writing RawBytes block , wptr=%v , num bytes = %v \n", wptr, len(block.RawBytes))
-			_, e = t.Write(block.RawBytes)
+			_, e := t.Write(block.RawBytes)
 			glog.V(4).Infof("Writing bytes = %v \n", block.RawBytes)
 			if e != nil {
-				glog.Fatal(e)
+				return e
 			}
 			wptr += int64(len(block.RawBytes))
 		} else {
